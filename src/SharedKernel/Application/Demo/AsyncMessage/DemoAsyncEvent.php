tmezynski/skeleton-s6

Add documentation

src/SharedKernel/Application/Demo/AsyncMessage/DemoAsyncEvent.php
@@ -1,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SharedKernel\Application\Demo\AsyncMessage;
+
+use SharedKernel\Application\Message\AbstractTraceableMessage;
+use SharedKernel\Application\Message\AsyncMessageInterface;
+
+final class DemoAsyncEvent extends AbstractTraceableMessage implements AsyncMessageInterface
+{
+    public function __construct(private readonly string $name, private readonly int $value)
+    {
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function value(): int
+    {
+        return $this->value;
+    }
+}

src/SharedKernel/Application/Demo/SyncMessage/DemoSyncEvent.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SharedKernel\Application\Demo\SyncMessage;
+
+use SharedKernel\Application\Message\AbstractTraceableMessage;
+use SharedKernel\Application\Message\SyncMessageInterface;
+
+final class DemoSyncEvent extends AbstractTraceableMessage implements SyncMessageInterface
+{
+    public function __construct(public readonly string $name, public readonly int $value)
+    {
+    }
+}

src/SharedKernel/Application/Message/TraceableStampFactory.php
@@ -1,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SharedKernel\Application\Message;
+
+use SharedKernel\Domain\Uuid;
+
+final readonly class TraceableStampFactory
+{
+    public function __construct(private string $producerName)
+    {
+    }
+
+    public function createFromPrevious(TraceableStamp $traceableStamp): TraceableStamp
+    {
+        return new TraceableStamp(
+            $this->producerName,
+            Uuid::generate(),
+            $traceableStamp->messageId,
+            $traceableStamp->correlationId,
+        );
+    }
+
+    public function create(): TraceableStamp
+    {
+        $uuid = Uuid::generate();
+
+        return new TraceableStamp($this->producerName, $uuid, $uuid, $uuid);
+    }
+}

tmezynski/php-skeleton-symfony

src/SharedKernel/Application/Demo/AsyncMessage/DemoAsyncEvent.php
@@ -1,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SharedKernel\Application\Demo\AsyncMessage;
+
+use SharedKernel\Application\Message\AbstractTraceableMessage;
+use SharedKernel\Application\Message\AsyncMessage;
+
+final class DemoAsyncEvent extends AbstractTraceableMessage implements AsyncMessage
+{
+    public function __construct(private readonly string $name, private readonly int $value)
+    {
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function value(): int
+    {
+        return $this->value;
+    }
+}

src/SharedKernel/Application/Demo/SyncMessage/DemoSyncEvent.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SharedKernel\Application\Demo\SyncMessage;
+
+use SharedKernel\Application\Message\AbstractTraceableMessage;
+use SharedKernel\Application\Message\SyncMessage;
+
+final class DemoSyncEvent extends AbstractTraceableMessage implements SyncMessage
+{
+    public function __construct(public readonly string $name, public readonly int $value)
+    {
+    }
+}

src/SharedKernel/Application/Message/TraceableStampFactory.php
@@ -1,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SharedKernel\Application\Message;
+
+use SharedKernel\Domain\Uuid;
+
+final readonly class TraceableStampFactory
+{
+    public function __construct(private string $producerName)
+    {
+    }
+
+    public function createFromPrevious(TraceableStamp $traceableStamp): TraceableStamp
+    {
+        return new TraceableStamp(
+            $this->producerName,
+            Uuid::generateRandom(),
+            $traceableStamp->messageId,
+            $traceableStamp->correlationId,
+        );
+    }
+
+    public function create(): TraceableStamp
+    {
+        $uuid = Uuid::generateRandom();
+
+        return new TraceableStamp($this->producerName, $uuid, $uuid, $uuid);
+    }
+}